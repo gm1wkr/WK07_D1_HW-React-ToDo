8413fb36eff6b71c5b0d0e6b60f85df4bba644c7
GM1WKR <gm1wkr@gmail.com>
2022-06-27 18:29:12 +0100

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    { name: "Buy shopping", priority: "high" },
+    { name: "Clean bathroom", priority: "low" },
+    { name: "Car's MOT", priority: "high" } 
+  ]);
+
+  const todoNodes = todos.map((todo, index) => {
+    return (
+      <li key={index} className={todo.priority === 'high' ? 'high-priority' : 'low-priority'}>
+        <span>{todo.name}</span>
+        <span>{todo.priority}</span>
+      </li>
+    )
+  });
+
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleToDoInput = (evt) => {
+    setNewTodo(evt.target.value);
+  }
+
+  const [newRadioValue, setNewRadioValue] = useState("low");
+
+  const handleRadioInputs = (evt) => {
+    setNewRadioValue(evt.target.value);
+    console.log('Radio VALUE: ', evt.target.value);
+  }
+
+  const saveTodo = (evt) => {
+    evt.preventDefault();
+    const copyTodos = [...todos];
+    copyTodos.push({name: newTodo, priority: newRadioValue});
+    setTodos(copyTodos);
+    setNewTodo("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>ToDo or ToDon't, that is the question.</h1>
+
+      <form onSubmit={saveTodo}>
+        <label htmlFor='new-todo'><span>Add a ToDo</span>
+          <input id='new-todo' type='text' value={newTodo} onChange={handleToDoInput} placeholder='Enter Todo Title' />
+        </label>
+        
+        <div>
+          <label htmlFor='priority-high'>High Priority</label>
+            <input type='radio' name='priority' value='high' onChange={handleRadioInputs} />
+        
+          <label htmlFor='priority-low'>Low Priority</label>
+            <input type='radio' name='priority' value='low' checked='checked' onChange={handleRadioInputs} />
+        </div>
+        <input type='submit' value='Add ToDo' />
+      </form>
+
+      <ul>
+        {todoNodes}
+      </ul>
+
     </div>
+
+    
+
+
   );
 }
 
